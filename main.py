3d7350eadcd53e9aed6254eefd7003f5b492e10a
akvergaray <akvergaray@baufest.com>
2021-08-29 16:18:20 -0500

    Configuracion heroku

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from runEatingPlan import runEatingPlan
+from decouple import config as config_decouple
+
 app = Flask(__name__)
 
 # Testing Route
@@ -12,5 +14,10 @@ def getProduct(total_calories):
     response = runEatingPlan (total_calories)
     return jsonify(response)
 
+
 if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+    if config_decouple('PRODUCTION', default=False):
+        app.run(debug=False, port=4000)
+    else: 
+        app.run(debug=True, port=4000)
+
